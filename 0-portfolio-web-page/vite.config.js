e952ce0fa3e825a120a447c0f017496f46ed728f
Simonas Danielius <s.danielius8@gmail.com>
2024-07-01 19:31:02 +0300

    Small bugfixes

--- 0-portfolio-web-page/vite.config.js
+++ 0-portfolio-web-page/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    svgr({
+      // Enabling SVGR for SVG files with ?react query
+      include: '**/*.svg?react'
+    }),
+  ],
+});
