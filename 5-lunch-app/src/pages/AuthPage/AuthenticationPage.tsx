132563a75af01c770703495915cd2e07ee97784f
Simonas Danielius <s.danielius8@gmail.com>
2024-07-21 19:13:52 +0300

    Adding information modals for website viewers

--- 5-lunch-app/src/pages/AuthPage/AuthenticationPage.tsx
+++ 5-lunch-app/src/pages/AuthPage/AuthenticationPage.tsx
@@ -5,10 +5,17 @@ import logo from '../../assets/static/logo/logo_horizontal.svg';
 import AuthTabs from './AuthTabs';
 import { RoutePath } from '../../types/navigationEnums';
 import Footer from '../../components/Footer/Footer';
+import { useEffect, useState } from 'react';
+import Dialog, { DialogSize } from '../../components/Dialog/Dialog';
 
 function AuthenticationPage() {
   const location = useLocation();
   const preselectedTab = location.pathname === RoutePath.REGISTER ? 1 : 0;
+  const [showDialog, setShowDialog] = useState(false)
+
+  useEffect(() => {
+    setShowDialog(true);
+  }, [])
 
   return (
     <div className={styles.container}>
@@ -25,6 +32,35 @@ function AuthenticationPage() {
       <div className={styles.footer}>
         <Footer />
       </div>
+      {
+        showDialog && (
+          <Dialog
+            title='Introduction to project'
+            size={DialogSize.LARGE}
+            primaryButtonText="Okay!"
+            onPrimaryButtonClick={() => setShowDialog(false)}
+            onClose={() => setShowDialog(false)}
+            children={(
+              <>
+                <p>
+                  <span>
+                    This project is a lunch app featuring a front-end built with React and TypeScript, and a JSON server for data management. For testing purposes, a user account has been created on the server. You can log in using the following credentials:
+                  </span>
+                  <br />
+                  <span><b>Login: frontender@gmail.com</b></span>
+                  <br />
+                  <span><b>Password: securepass123</b></span>
+                  <br />
+                  <span>Additionally, you can create a new user account via the registration tab.</span>
+                  <br />
+                  <span><b>For testing</b>, it is recommended to use the pre-existing user account as it has a pre-set balance for ordering meals.
+                  </span>
+                </p>
+              </>
+            )}
+          />
+        )
+      }
     </div>
   );
 }
--- 5-lunch-app/src/pages/FoodMenuPage/FoodMenuPage.tsx
+++ 5-lunch-app/src/pages/FoodMenuPage/FoodMenuPage.tsx
@@ -1,13 +1,16 @@
 import { useState, useEffect } from 'react';
-import Dialog, { DialogIcon } from '../../components/Dialog/Dialog';
+import Dialog, { DialogIcon, DialogSize } from '../../components/Dialog/Dialog';
 import FoodCardsLayout from './FoodCardsLayout';
 import { STOP_ORDERS_HOUR, isWorkday } from '../../utils/dateUtils';
 import Header from '../../components/Header/Header';
 
 export default function FoodMenuPage() {
   const [showDialog, setShowDialog] = useState(false);
+  const [showInfoDialog, setShowInfoDialog] = useState(false)
 
   useEffect(() => {
+    setShowInfoDialog(true)
+
     const now = new Date();
     const stopOrdersDate = new Date(
       now.getFullYear(),
@@ -45,6 +48,34 @@ export default function FoodMenuPage() {
         </Dialog>
       )}
       <FoodCardsLayout />
+      {
+        showInfoDialog && (
+          <Dialog
+            title='Information about meals'
+            size={DialogSize.LARGE}
+            primaryButtonText="Okay!"
+            onPrimaryButtonClick={() => setShowInfoDialog(false)}
+            onClose={() => setShowInfoDialog(false)}
+            children={(
+              <>
+                <p>
+                  <span>
+                    Upon logging in, you will be redirected to the meals page. This page showcases all available meals for ordering and <b>has a few additional edge cases</b>:
+                  </span>
+                  <br />
+                  <span>
+                    1. Since lunch is around 12 PM, this app allows meal orders until 11 AM on the current day. After 11 AM, a modal will appear providing necessary information and prompting you to refresh the page. Once refreshed, meals for the current day will no longer be displayed. Additionally, previous day tabs will be disabled. However, you can still switch to upcoming days (e.g., if you check on Tuesday at 1 PM, Monday and Tuesday tabs will be unavailable).
+                  </span>
+                  <br />
+                  <span>
+                    2. On Fridays, meals are free, as lunch is paid for by the company. However, on Fridays, you can only order one side and one main dish.
+                  </span>
+                </p>
+              </>
+            )}
+          />
+        )
+      }
     </div>
   );
 }
